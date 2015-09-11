eeb781b696ef62380848b70e3c309d8600b44a6e
epiapple <darshlin714@gmail.com>
2015-09-10 22:53:26 -0700

    Problems with mergesort

---
+++ LabFourteen/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabFourteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Sort<int> sorter = new Sort<int>();
+
+            list.Add(55);
+            list.Add(45);
+            list.Add(32);
+            list.Add(61);
+            list.Add(88);
+            list.Add(92);
+            list.Add(4);
+            list.Add(74);
+            list.Add(123);
+            list.Add(16);
+            list.Add(861);
+            list.Add(64);
+            list.Add(49);
+            list.Add(34);
+            list.Add(67);
+            list.Add(478);
+            list.Add(58);
+            list.Add(12);
+
+            sorter.MergeSort(list);
+            //for (int i = 0; i < list.Count; i++)
+            //{
+            //    Console.WriteLine(list[i]);
+            //}
+
+                Console.ReadKey();
+        }
+    }
+}
---
+++ LabFourteen/Sort.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabFourteen
+{
+    class Sort<T> where T : IComparable
+    {
+        //List<T> list = new List<T>();
+        public void InsertionSort(List<T> line)
+        {
+            List<T> list = new List<T>(line);
+            T data;
+            int j;
+            for (int i = 1; i < list.Count; i++)
+            {
+                data = list[i];
+                j = i;
+                while (j > 0 && list[j-1].CompareTo(data) >0)
+                {
+                    list[j] = list[j - 1];
+                    j = j - 1;
+                    list[j] = data;
+                }
+            }
+
+            for(int i = 0; i < list.Count; i++) 
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+        private void Merge(List<T> Left, List<T> Right, List<T> list)
+        {
+            int l = Left.Count;
+            int r = Right.Count;
+
+            int x = 0;
+            int y = 0;
+            int z = 0;
+
+            while (x < l && y < r)
+            {
+                if (Left[x].CompareTo(Right[y]) < 0)
+                {
+                    list[z] = Left[x];
+                    z++;
+                    x++;
+                }
+                else
+                {
+                    list[z] = Right[y];
+                    z++;
+                    y++;
+                }
+            }
+
+        }
+        public void MergeSort(List<T> line)
+        {
+            List<T> list = new List<T>(line);
+
+            T data;
+            int length = list.Count;
+            if (length < 2)
+            {
+                return;
+            }
+
+            int mid = length / 2;
+            List<T> Left = new List<T>(mid);
+            List<T> Right = new List<T>(length - mid);
+
+            for (int i = 0; i < mid; i++)
+            {
+                Left.Add(list[i]);
+               
+            }
+            for (int i = mid; i < length; i++)
+            {
+                data = list[i];
+                Right.Add(data);
+            }
+
+            MergeSort(Left);
+            MergeSort(Right);
+            Merge(Left, Right, list);
+        }
+        
+        public void HeapSort()
+        {
+
+        }
+        public void QuickSort()
+        {
+
+        }
+        public void ListSort()
+        {
+
+        }
+    }
+}
